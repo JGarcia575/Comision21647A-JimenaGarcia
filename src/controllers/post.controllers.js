d05374873ca87cc7507ef7158f3c8297aba9ec02
JGarcia575 <jimenagarcia575@gmail.com>
2023-09-21 17:14:25 -0300

    controlador actualizado

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -21,50 +21,68 @@ crudPost.indexCrearPost = async (req, res) => {
 crudPost.crearPost = async (req, res) => {
     const { titulo, contenido, url_imagen } = req.body;
 
-    const post = {
-        titulo: titulo,
-        contenido: contenido,
-        url_imagen: url_imagen
-    };
-
     try {
+        const post = {
+            titulo: titulo,
+            contenido: contenido,
+            url_imagen: url_imagen
+        };
+
         const nuevoPost = await Post.create(post);
-        res.redirect('/posts');
+
+        if (nuevoPost) {
+            res.redirect('/posts');
+        } else {
+            res.send({message: 'No se pudo agregar la publicación'});
+        }        
     } catch (error) {
-        res.send({message:'Error al crear el post' + error});  
-    };
+        res.send({message: 'Error al crear la publicación' + error});
+    }
 };
 
 //CONTROLADOR PARA IR A LA RUTA DE FORMULARIO DE ACTUALIZACIÓN DE POST
 crudPost.indexModificarPost = async (req, res) => {
     const { id } = req.params;
 
-    const post = await Post.findOne({ where: { id: id }});
+    try {
+        const post = await Post.findOne({ where: { id: id }});
 
-    res.render('editPost', { post : post });
+        if (post) {
+            res.render('editPost', { post : post });
+        } else {
+            res.send({message: 'No se encontró el post'});
+        }
+    } catch (error) {
+        res.send({message: 'Error al buscar el post' + error});
+    }
+    //const post = await Post.findOne({ where: { id: id }});
+
+    //res.render('editPost', { post : post });
 };
 
 // CONTROLADOR PARA ACTUALIZAR UN POST
 crudPost.actualizarPost = async (req, res) => {
     const { id, titulo, contenido, url_imagen} = req.body
 
-    //const post = Post.findOne({ where: { id: id } });
-
     try {
-            const postActualizar = await Post.update({ 
-                titulo: titulo,
-                contenido: contenido, 
-                url_imagen: url_imagen,
-                updatedAt: new Date().toDateString()                  
-              },
-              { where: {
-                  id: id
-              }});
+        const postActualizar = await Post.update({ 
+            titulo: titulo,
+            contenido: contenido, 
+            url_imagen: url_imagen,
+            updatedAt: new Date()     //new Date().toDateString()              
+          },
+          { where: {
+              id: id
+          }});
+        
+        if (postActualizar) {
             res.redirect('/posts');
+        } else {
+            res.send({message: 'No se pudo actualizar la publicación'});
+        }           
         } catch (error) {
-            res.send({ message:'Error al actualizar el post determinado' });
+            res.send({ message:'Error al actualizar el post' });
         };
-    //};
 };
 
 // CONTROLADOR PARA BORRAR UN POST
@@ -78,10 +96,9 @@ crudPost.borrarPost = async (req, res) => {
             }
         });
         if (postEliminar) {
-            //res.send({ message: '¡Post eliminado exitosamente!' });
             res.redirect('/posts');
         } else {
-            res.send({ message: 'No existe el id del post a eliminar' });
+            res.send({ message: 'No se pudo borrar el post'});
         }
     } catch (error) {
         res.send({ message: 'Error al tratar de eliminar el post' })
